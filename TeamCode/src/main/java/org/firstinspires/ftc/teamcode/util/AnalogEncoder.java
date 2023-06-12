d06da1723fdf25fa8439642c4390acb8978ab58e
Nakulj11 <nakulsaliljoshi@gmail.com>
2023-06-12 13:59:53 -0400

    analog encoder changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AnalogEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AnalogEncoder.java
@@ -6,11 +6,8 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 public class AnalogEncoder {
 
     private final static double RANGE = 3.3;
-    private final static boolean VALUE_REJECTION = false;
 
     private boolean reverse;
-
-    private double pastPosition = 1;
 
     private AnalogInput encoder;
     private double zero;
@@ -31,29 +28,27 @@ public class AnalogEncoder {
     public void zero(){
         zero = getCurrentPosition();
     }
-    public void setInverted(boolean invert){
-        reverse = invert;
+    public void reverse(boolean reverse){
+        this.reverse = reverse;
     }
     public boolean getDirection() {
         return reverse;
     }
 
     public double getCurrentPosition() {
-        double pos = Angle.norm((!reverse ? 1 - getVoltage() / range : getVoltage() / range) * Math.PI*2 - zero);
+        double pos;
 
-        if(!VALUE_REJECTION || Math.abs(Angle.normDelta(pastPosition)) > 0.1 || Math.abs(Angle.normDelta(pos)) < 1) pastPosition = pos;
-        return pastPosition;
+        if(reverse){
+            pos = Angle.norm((1 - getVoltage() / range) * 2 * Math.PI - zero);
+        }else{
+            pos = Angle.norm((getVoltage() / range) * 2 * Math.PI - zero);
+        }
+
+        return pos;
     }
 
     public AnalogInput getEncoder() {
         return encoder;
-    }
-
-    public static double reLinearize(double v){
-        double v2 = 3.3-v;
-        double alpha = v2 * ((3.3 * v2) - 16.335);
-        double beta = (v2 * v2) - (3.3 * v2) - 5.445;
-        return 3.3 - (alpha / beta);
     }
 
     public double getVoltage(){
