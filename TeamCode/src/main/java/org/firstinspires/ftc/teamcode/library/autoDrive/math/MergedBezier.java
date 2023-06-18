e8823c1781cb2fab684920f98deee9c7aa14d2b0
Nakulj11 <nakulsaliljoshi@gmail.com>
2023-06-18 14:44:44 -0400

    some math corrections

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/autoDrive/math/MergedBezier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/autoDrive/math/MergedBezier.java
@@ -5,35 +5,60 @@ public class MergedBezier extends Bezier{
 
     Bezier[] curves;
 
+    double[] mergePoints;
+
     public MergedBezier(Bezier... curves){
         this.curves = curves;
+
+        mergePoints = new double[curves.length];
+        double len = approximateLength();
+        for(int i=0;i< mergePoints.length-1;i++){
+            mergePoints[i] = curves[i].approximateLength()/len + ((i==0) ? 0 : mergePoints[i-1]);
+        }
+
+        mergePoints[mergePoints.length-1] = 1;
+
         generateCurve();
     }
 
     @Override
     public Point getPoint(double t){
+
+
+
         if(t>=1) {
             return curves[curves.length-1].getPoint(1);
         }else {
-            return curves[(int)(curves.length*t)].getPoint((curves.length*t)-((int)(curves.length*t)));
+            int i=0;
+            while (t > mergePoints[i]){i++;}
+            return curves[i].getPoint((i==0) ? (t/mergePoints[i]):((t-mergePoints[i-1])/(mergePoints[i]-mergePoints[i-1])));
         }
     }
 
     @Override
     public double getHeading(double t){
+
+
+
         if(t>=1) {
             return curves[curves.length-1].getHeading(0);
         }else {
-            return curves[(int)(curves.length*t)].getHeading(0);
+            int i=0;
+            while (t > mergePoints[i]){i++;}
+            return curves[i].getHeading((i==0) ? (t/mergePoints[i]):((t-mergePoints[i-1])/(mergePoints[i]-mergePoints[i-1])));
         }
     }
 
     @Override
     public Point getDerivative(double t){
+
+
         if(t>=1) {
             return curves[curves.length-1].getDerivative(1);
         }else {
-            return curves[(int)(curves.length*t)].getDerivative((curves.length*t)-((int)(curves.length*t)));
+            int i=0;
+            while (t > mergePoints[i]){i++;}
+            return curves[i].getDerivative((i==0) ? (t/mergePoints[i]):((t-mergePoints[i-1])/(mergePoints[i]-mergePoints[i-1])));
         }
     }
 
@@ -57,4 +82,6 @@ public class MergedBezier extends Bezier{
     public Point getEndPoint() {
         return curves[curves.length-1].getEndPoint();
     }
+
+
 }
